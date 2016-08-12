c1a25250c137b013e11807b61f21b8d228d3ba74
Micky Shaked <micky.shaked@gmail.com>
2016-08-12 16:21:04 -0700

    updated dev schema, ignore methods

--- database/models/devModel.js
+++ database/models/devModel.js
@@ -2,10 +2,19 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const devSchema = new Schema({
-  'devID':	Number,
-  'devUserName':	String,
-  'devPassword':	String,
-  'devDB':	String,
+	devID: {
+		type: String,
+		required: true
+	},
+	devUser: {
+		type: String,
+		required: true
+	},
+	devPW: {
+		type: String,
+		required: true
+	},
+	devDB:	String
 });
 
 const Dev = mongoose.model('Dev', devSchema);
