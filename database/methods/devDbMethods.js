50353dcb511e5a49bb060382152644eb6076a86d
Chandra Lindy <chandra.lindy@gmail.com>
2016-08-16 20:41:08 -0700

    extracted out createDb method into own file

---
+++ database/methods/devDbMethods.js
@@ -0,0 +1,8 @@
+const Devs = require('../models/devModel');
+
+function createDb(req, res){
+	, function(err, dev){
+		// define new collection with devID and devDB
+		// predetermined schema?
+	})
+}
