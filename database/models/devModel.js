01d8c3209d472952c4949d5869cfb46f66dac4be
Chandra Lindy <chandra.lindy@gmail.com>
2016-08-17 14:06:49 -0700

    fixed devModel schema and worked on testing devDbMethod update function

--- database/models/devModel.js
+++ database/models/devModel.js
@@ -1,17 +1,20 @@
 const mongoose = require('mongoose');
+
 const Schema = mongoose.Schema;
 
 const devSchema = new Schema({
-  userName: String,
-  password: String,
-  db: [{
-    id: { type: String, default: 'null' },
-    name: { type: String, default: 'null' },
-    collection: {
+  userName: { type: String, required: true },
+  password: { type: String, required: true },
+  database: { type: Array,
+    default: {
+      id: { type: String, default: 'null' },
       name: { type: String, default: 'null' },
-      schema: { type: String, default: 'null' }
-    },
-  }],
+      collection: {
+        name: { type: String, default: 'null' },
+        schema: { type: String, default: 'null' },
+      },
+    }
+  },
 });
 
 module.exports = mongoose.model('Dev', devSchema);
