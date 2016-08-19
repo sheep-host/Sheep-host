54b490ec26d86a9690f656fb31baa07c8dd6bf10
Micky Shaked <micky.shaked@gmail.com>
2016-08-18 21:38:38 -0700

    route to create devDB added to server.js

--- database/methods/devDbMethods.js
+++ database/methods/devDbMethods.js
@@ -19,14 +19,12 @@ function updateDevProfile(req, res, next){
 
   Devs.findOneAndUpdate(query, { $set: data }, function(err, dev) {
     if (err) throw err;
-    console.log(dev);
     next();
   });
 }
 
 // new DB spooled up using id from dev profile and chosen db name
 function createDevDB(req, res, next) {
-  console.log('inside createDevDB');
   const devDB = mongoose.createConnection(uri + req.body.dbId + '_' + req.body.dbName);
   const devModel = devDB.model('label', new mongoose.Schema({
     createdBy: String
@@ -35,7 +33,6 @@ function createDevDB(req, res, next) {
   devModel({
     createdBy: req.body.userName
   }).save(function (err, results) {
-    console.log('model saved', results);
     if (err) throw err;
     res.json(results);
   });
