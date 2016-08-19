8b1a1e01131cc778d372685a3a44cd5ed6a4607e
Chandra Lindy <chandra.lindy@gmail.com>
2016-08-18 17:14:33 -0700

    fix schema in devModel

--- database/models/devModel.js
+++ database/models/devModel.js
@@ -2,19 +2,21 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+const Collections = new Schema({
+  name: { type: String, default: 'null' },
+  devSchema: { type: String, default: 'null' },
+});
+
+const Database = new Schema({
+  id: { type: String, default: 'null' },
+  name: { type: String, default: 'null' },
+  collections: [Collections],
+});
+
 const devSchema = new Schema({
   userName: { type: String, required: true },
   password: { type: String, required: true },
-  database: { type: Array,
-    default: {
-      id: { type: String, default: 'null' },
-      name: { type: String, default: 'null' },
-      collection: {
-        name: { type: String, default: 'null' },
-        schema: { type: String, default: 'null' },
-      },
-    }
-  },
+  database: [Database],
 });
 
 module.exports = mongoose.model('Dev', devSchema);
