0096c76b3595ca109f575e077cdf2be09ea04925
Chandra Lindy <chandra.lindy@gmail.com>
2016-08-22 15:58:43 -0700

    heroku fix

--- server/server.js
+++ server/server.js
@@ -78,6 +78,6 @@ app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('listening on port 3000');
 });
