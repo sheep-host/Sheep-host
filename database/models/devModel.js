718026c89c6338ec45250c4e49c8dd0cb52cc5e8
Micky Shaked <micky.shaked@gmail.com>
2016-08-24 19:17:42 -0700

    var over const

--- database/models/devModel.js
+++ database/models/devModel.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const SALT_WORK_FACTOR = 10;
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+var SALT_WORK_FACTOR = 10;
 
 var Schema = mongoose.Schema;
 
