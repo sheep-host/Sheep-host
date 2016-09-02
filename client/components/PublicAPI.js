f3c53d1f9453646e1158ab10f880aa4a09b2acdd
Chandra Lindy <chandra.lindy@gmail.com>
2016-09-02 09:10:50 -0700

    public api post cors issues resolved and functioning

--- client/components/PublicAPI.js
+++ client/components/PublicAPI.js
@@ -8,13 +8,13 @@ const PublicAPI = React.createClass({
           <pre>
             <code>
               &lt;script src="https://npmcdn.com/axios/dist/axios.min.js"&gt;&lt;/script&gt;{'\n'}
-              &lt;script src="https://sheep.host/public_api"&gt;&lt;/script&gt;{'\n'}
+              &lt;script src="http://sheep.host/public_api"&gt;&lt;/script&gt;{'\n'}
               &lt;script&gt;{'\n'}
                   {'  '}// Initialize Sheep.host{'\n'}
                   {'  '}var config = &#123;{'\n'}
                       {'    '}id: {this.props.devId},{'\n'}
                       {'    '}authorization: {this.props.authKey},{'\n'}
-                      {'    '}url: "https://sheep.host/api/"{'\n'}
+                      {'    '}url: "http://sheep.host/api/"{'\n'}
                   {'  '}&#125;{'\n'}
                   {'  '}sheep.dontSleep(config);{'\n'}
               &lt;/script&gt;{'\n'}
