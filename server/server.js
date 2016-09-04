f91ec7336303f2b3141bf634adfecb132e6d3dc6
s1akr <chandra.lindy@gmail.com>
2016-09-04 01:04:45 +0000

    work on public api continues, changes made to allow put api requests for dev api

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 
 app.use('/api', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Method", "GET, POST, HEAD");
+  res.header("Access-Control-Allow-Method", "GET, POST, HEAD, OPTIONS, PUT");
   res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization");
   next();
 });
